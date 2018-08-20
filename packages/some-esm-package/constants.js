f62bd04203a434d8859af498809058a474109063
Charandas Batra-Daitch <charandas108@gmail.com>
2018-08-20 12:47:35 -0400

    Remove unnecessary file

--- packages/some-esm-package/constants.js
+++
@@ -1,9 +0,0 @@
-export const MODEL_NAMES = Object.freeze({
-  'SOMETHING': 'something',
-  'SOME_OTHER_THING': 'someOtherThing'
-})
-
-export const MODEL_NAMES_MAP = new Map([
-  ['SOMETHING', 'something'],
-  ['SOME_OTHER_THING', 'someOtherThing']
-])
